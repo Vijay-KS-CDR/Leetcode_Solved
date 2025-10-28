85a08ad007450e525fe6606335b81c30ec3278df
VIJAY K S <ksvijay711@gmail.com>
2025-10-28 12:04:55 +0530

    Time: 1 ms (96.89%), Space: 40.9 MB (85.27%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,15 +1,5 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int stairs=0;
-        for(int i=1;i<=n;i++){
-            if((n/i)>=1){
-                stairs++;
-                n=n-i;
-            }
-            else{
-                break;
-            }
-        }
-        return stairs;
+        return (int)((Math.sqrt(1+8L*n)-1)/2);
     }
 }
