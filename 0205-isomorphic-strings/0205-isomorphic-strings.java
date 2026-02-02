2be576a9e48e0484a7bf149a0943eaa8e644c152
VIJAY K S <ksvijay711@gmail.com>
2026-02-02 15:52:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character> map=new HashMap();
+        for(int i=0;i<s.length();i++){
+            map.putIfAbsent(s.charAt(i),t.charAt(i));
+            char k=map.get(s.charAt(i));
+            if(k!=t.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
