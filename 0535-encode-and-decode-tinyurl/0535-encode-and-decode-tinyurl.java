f5aa2fd8123453ad8472dc2bc4fc609d5fa00e55
VIJAY K S <ksvijay711@gmail.com>
2026-02-16 10:34:29 +0530

    Time: 0 ms (100%), Space: 44.1 MB (81.18%) - LeetHub

---
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -0,0 +1,16 @@
+public class Codec {
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String u) {
+        return u;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String u) {
+        return u;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
