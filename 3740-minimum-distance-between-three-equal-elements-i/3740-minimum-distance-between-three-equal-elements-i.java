4c1a6ce3cfbbd8ec9b99f631f7bf143139ce0d64
VIJAY K S <ksvijay711@gmail.com>
2026-04-10 12:59:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
@@ -4,8 +4,8 @@ class Solution {
             return -1;
         }
         int min=Integer.MAX_VALUE;
-        for(int i =0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
+        for(int i=0;i<nums.length-2;i++){
+            for(int j=i+1;j<nums.length-1;j++){
                 for(int k=j+1;k<nums.length;k++){
                     if(nums[i]==nums[j] && nums[j]==nums[k]){
                         min=Math.min(min,Math.abs(i-j)+Math.abs(j-k)+Math.abs(k-i));
