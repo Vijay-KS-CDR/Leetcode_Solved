97c9e51deca7c6cc597ecdb5f1b2f777f37d11b9
VIJAY K S <ksvijay711@gmail.com>
2026-06-11 15:21:29 +0530

    Time: 1 ms (99.27%), Space: 42.8 MB (14.63%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -3,11 +3,11 @@ class Solution {
         while(n!=0 & n%5==0){
             n=n/5;
         }
-        while(n%3==0 && n!=0){
-            n=n/3;
-        }
         while(n%2==0 && n!=0){
             n=n/2;
+        }
+        while(n%3==0 && n!=0){
+            n=n/3;
         }
         return n==1;
     }
