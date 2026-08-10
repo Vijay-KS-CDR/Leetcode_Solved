59188028bc269d803900a1cbf47e56a1c3fe3775
VIJAY K S <ksvijay711@gmail.com>
2026-08-10 11:56:55 +0530

    Time: 367 ms (82.6%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,1 +1,4 @@
-select score, DENSE_RANK() over (order by score desc) as rank from Scores order by score desc; 
+select score,
+DENSE_RANK() over (order by score desc) as rank
+from Scores
+order by score desc; 
