4b5d149b4b7c22c6688061272b32c99b23cb4b91
VIJAY K S <ksvijay711@gmail.com>
2026-08-13 09:38:36 +0530

    Time: 418 ms (7.85%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,4 +1,2 @@
 # Write your MySQL query statement below
-select * from Cinema
-where id%2<>0 and description not like "b%"
-order by rating desc;
+select * from Cinema where mod(id,2) <> 0 and description <> 'boring' order by  rating desc;
