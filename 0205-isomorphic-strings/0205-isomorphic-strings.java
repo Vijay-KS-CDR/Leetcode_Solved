1f8921e7e559f53ac0c12065e4f58b8b822fea6d
VIJAY K S <ksvijay711@gmail.com>
2026-09-24 10:26:34 +0530

    Time: 38 ms (5.69%), Space: 44.6 MB (5.4%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,18 +1,24 @@
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,Character> map=new HashMap();
-        for(int i=0;i<s.length();i++){
-            map.putIfAbsent(s.charAt(i),t.charAt(i));
-            char k=map.get(s.charAt(i));
-            if(k!=t.charAt(i)){
-                return false;
+    public boolean isIsomorphic(String a, String b) {
+        HashMap<Character,Character> map1 = new HashMap<>();
+        HashMap<Character,Character> map2 = new HashMap<>();
+        for(int i=0;i<a.length();i++){
+            if(i==a.length() || i==b.length()){
+                break;
             }
+            if(map1.containsKey(a.charAt(i))){
+                if(map1.get(a.charAt(i))!=b.charAt(i)){
+                    return false;
+                }
+            }
+            if(map2.containsKey(b.charAt(i))){
+                if(map2.get(b.charAt(i))!=a.charAt(i)){
+                    return false;
+                }
+            }
+            map1.putIfAbsent(a.charAt(i),b.charAt(i));
+            map2.putIfAbsent(b.charAt(i),a.charAt(i));
         }
-        HashSet<Character> set=new HashSet<Character>();
-        for(char ch:map.values()){
-            set.add(ch);
-        }
-
-        return set.size()==map.size();
+        return true;
     }
 }
